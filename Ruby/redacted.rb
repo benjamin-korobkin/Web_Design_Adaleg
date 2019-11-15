5564467d00f950aa80d704d55303efb6d093ae05
Benjamin J Korobkin <38887228+benjamin-korobkin@users.noreply.github.com>
2019-11-14 22:09:32 -0500

    Update redacted.rb

--- Ruby/redacted.rb
+++ Ruby/redacted.rb
@@ -3,6 +3,7 @@ story = gets.chomp
 
 arr = story.split(" ")
 
+# SOLUTION 1
 # for x in arr do 
 #   if x.include?("e") then
 #     x = "CENSORED"
@@ -10,6 +11,7 @@ arr = story.split(" ")
 #   print "#{x} "
 # end
 
+# SOLUTION 2
 for x in 0...arr.length do
   if arr[x].include?("e") then
     arr[x] = "CENSORED"
