405f256a78963fd5f6ba73de245db257daaa2f52
Benjamin J Korobkin <38887228+benjamin-korobkin@users.noreply.github.com>
2019-12-29 11:30:59 -0500

    Add files via upload
    Includes challenge assignment for text adventure project

---
+++ Ruby/classes/room.rb
@@ -0,0 +1,18 @@
+# Create a Room with classes
+
+# You will be creating a building strucutre that allows
+# a user to navigate from room to room.
+# Focusing on the room first, think about what attributes a 
+# room has.
+# From our example
+# exits, description, roomID (to uniquely ID every room)
+
+# Examples of roomID could be kitchen, laundry room, dining room,
+# library, etc...
+
+# Exercise 1: Create a class called Room that has the attributes
+# described above: exits, description, roomID/Name
+
+# Exercise 2: Create an object called classroom using your
+# Room class. The classroom needs 1 exit, a description and
+# roomID (or name) called "classroom"
