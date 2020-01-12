b907e72c691d19dd1fe1ba6f2e9c698accaa4d77
Benjamin J Korobkin <38887228+benjamin-korobkin@users.noreply.github.com>
2020-01-11 20:56:13 -0500

    Add files via upload
    Fill in the places where it says TODO

---
+++ Ruby/classes/class_proj/Character_todo1.rb
@@ -0,0 +1,53 @@
+require_relative 'item.rb'
+
+class Character
+	def initialize()
+		@stuff = []
+	end
+
+	def addItem(item)
+		@stuff.push(item)
+	end
+
+	def removeItem(item)
+		# TODO: remove the item from the player's inventory.
+	end
+
+	def hasItem(item)
+		# TODO: return true if the player's inventory contains
+		# the parameterized item. Return false otherwise.
+	end
+
+	def getItems
+		itemString = ""
+		@stuff.each do |item|
+			itemString += "#{item.name}: #{item.description}\n"
+		end 
+		return itemString
+	end
+
+end
+
+# Set up the items
+hammer = Item.new("Hammer", "A strong hammer")
+bow = Item.new("Bow","Flexible and agile")
+
+# Set up the character
+bob = Character.new()
+bob.addItem(hammer)
+bob.addItem(bow)
+puts "Inventory:\n#{bob.getItems}"
+puts "Bob has the hammer: #{bob.hasItem(hammer)}"
+puts "Bob has the bow: #{bob.hasItem(bow)}"
+puts "*****************"
+bob.removeItem(hammer)
+puts "Hammer removed..."
+puts "Inventory:\n#{bob.getItems}"
+puts "Bob has the hammer: #{bob.hasItem(hammer)}"
+puts "Bob has the bow: #{bob.hasItem(bow)}"
+puts "*****************"
+bob.removeItem(bow)
+puts "Bow removed..."
+puts "Inventory:\n#{bob.getItems}"
+puts "Bob has the hammer: #{bob.hasItem(hammer)}"
+puts "Bob has the bow: #{bob.hasItem(bow)}"
